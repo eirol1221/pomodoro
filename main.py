69dbae33b89fb9746aceebecede9147ec8448fe5
Lorie Lie Cubid <eirol1221@gmail.com>
2023-11-03 21:28:06 +0800

    Updated UI with image buttons, grid placement, and added functionality to display at the top of the other windows,

--- main.py
+++ main.py
@@ -1,49 +1,71 @@
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 1
-LONG_BREAK_MIN = 1
+TOMATO = "#F05D40"
+CINNABAR = "#EF4C38"
+GREEN = "#328F43"
+YELLOW = "#FFF8C9"
+FONT_NAME = "Comic Sans MS"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 
 reps = 0
 checks = ""
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-def reset_time():
-    global reps, checks
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps, checks, timer
+
+    btn_start.config(state=NORMAL)
+    btn_reset.config(state=DISABLED)
+
+    window.after_cancel(timer)
     reps = 0
     checks = ""
-    lbl_timer.config(text="Timer")
+    lbl_check.config(text=checks)
+    lbl_timer.config(text="Pomodoro Timer", fg=GREEN)
     canvas.itemconfig(timer_text, text="00:00")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
+
+    print(reps)
+    if reps == 8:
+        window.after_cancel(timer)
+        return
+
+    window.attributes('-topmost', 1)
+    window.attributes('-topmost', 0)
+
     count = 0
     reps += 1
 
-    if reps == 1 or reps == 3 or reps == 5 or reps == 7:
-        lbl_timer.config(text="WORK")
-        count = WORK_MIN
-    elif reps == 2 or reps == 4 or reps == 6:
-        lbl_timer.config(text="BREAK", fg=PINK)
-        count = SHORT_BREAK_MIN
-    elif reps == 8:
-        lbl_timer.config(text="BREAK", fg=RED)
-        count = LONG_BREAK_MIN
+    btn_start.config(state=DISABLED)
+    btn_reset.config(state=NORMAL)
 
+    if reps % 2 == 1:
+        text = "Time to Work 💪"
+        fg = GREEN
+        count = WORK_MIN
+    elif reps == 8:
+        text = "Long Break 😴"
+        fg = CINNABAR
+        count = LONG_BREAK_MIN
+    elif reps % 2 == 0:
+        text = "Short Break ☕"
+        fg = TOMATO
+        count = SHORT_BREAK_MIN
+
+    lbl_timer.config(text=text, fg=fg)
     count_down(count)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
 def count_down(min_count, sec_count=0):
-    global checks, reps
-    print(type(min_count))
+    global checks, reps, timer
 
     if sec_count < 10:
         canvas.itemconfig(timer_text, text=f"{min_count}:0{sec_count}")
@@ -51,40 +73,45 @@ def count_down(min_count, sec_count=0):
         canvas.itemconfig(timer_text, text=f"{min_count}:{sec_count}")
 
     if min_count >= 0 and sec_count > 0:
-        window.after(100, count_down, min_count, sec_count - 1)
-    elif min_count == 0 and sec_count == 0:
+        timer = window.after(10, count_down, min_count, sec_count - 1)
+    elif min_count > 0 and sec_count == 0:
+        timer = window.after(10, count_down, min_count - 1, sec_count + 59)
+    else:
         canvas.itemconfig(timer_text, text=f"{min_count}:0{sec_count}")
-        if reps == 1 or reps == 3 or reps == 5 or reps == 7:
-            checks += "✔"
+        if reps % 2 == 1:
+            checks += "🍅"
             lbl_check.config(text=checks)
         start_timer()
-    elif sec_count == 0:
-        window.after(100, count_down, min_count - 1, sec_count + 59)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=30, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=350, height=350, bg=YELLOW, highlightthickness=0, borderwidth=0)
 tomato = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato)
-timer_text = canvas.create_text(103, 135, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.grid(row=1, column=1)
+canvas.create_image(175, 175, image=tomato)
+timer_text = canvas.create_text(175, 220, text="00:00", fill="white", font=(FONT_NAME, 36, "bold"))
+canvas.grid(row=1, column=0, rowspan=2, ipadx=20, sticky="ew")
 
 # Buttons
-btn_start = Button(text="Start", font=("Arial", 10, "bold"), bg="white", highlightthickness=0, command=start_timer)
-btn_start.grid(row=2, column=0)
+start_img = PhotoImage(file="btn-start.png")
+btn_start = Button(image=start_img, bg=YELLOW, highlightthickness=0, borderwidth=0, command=start_timer)
+btn_start.grid(row=1, column=1, sticky='s')
 
-btn_reset = Button(text="Reset", font=("Arial", 10, "bold"), bg="white", highlightthickness=0, command=reset_time)
-btn_reset.grid(row=2, column=2)
+reset_img = PhotoImage(file="btn-reset.png")
+btn_reset = Button(image=reset_img, bg=YELLOW, highlightthickness=0, borderwidth=0, command=reset_timer, state=DISABLED)
+btn_reset.grid(row=2, column=1, sticky='ns')
+
+# pause_img = PhotoImage(file="btn-pause.png")
+# btn_pause = Button(image=pause_img, bg=YELLOW, highlightthickness=0, borderwidth=0, command=pause_timer, state=DISABLED)
+# btn_pause.grid(row=3, column=1, sticky='e', ipady=0)
 
 # Labels
-lbl_timer = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
-lbl_timer.grid(row=0, column=1)
+lbl_timer = Label(text="Pomodoro Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+lbl_timer.grid(row=0, column=0, columnspan=2, sticky='ew')
 
-lbl_check = Label(text="", font=(FONT_NAME, 12, "bold"), fg=GREEN, bg=YELLOW)
-lbl_check.grid(row=3, column=1)
-
+lbl_check = Label(font=(FONT_NAME, 16, "bold"), fg=TOMATO, bg=YELLOW, pady=20)
+lbl_check.grid(row=3, column=0, sticky="ew")
 
 window.mainloop()
